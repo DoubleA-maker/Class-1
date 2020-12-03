c769d8a7d67b123295ba2378f7095bafd4937d1b
DoubleA-maker <75404714+DoubleA-maker@users.noreply.github.com>
2020-12-02 19:44:02 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function flipimage(){
+  document.getElementById("image1").
+  src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ";}
+function flipimage2(){
+  document.getElementById("image2").
+  src="https://images.unsplash.com/photo-1544219111-6dca1d644520?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ"; 
+  }
